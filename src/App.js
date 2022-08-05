5668f418c6cd057e8beb4cf438cdcda49943de7d
willcordeiro <willcordeiro800@gmail.com>
2022-08-05 14:47:46 -0300

    added routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import "./App.css";
 const App = () => {
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
@@ -20,6 +21,50 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div>Sidebar w-0</div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? " md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              {" "}
+              Navbar
+            </div>
+          </div>
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" element="Ecommerce" />
+            <Route path="/ecommerce" element="Ecommerce " />
+
+            {/*pages */}
+            <Route path="/orders" element="Oders" />
+            <Route path="/employees" element="Employees" />
+            <Route path="/cystomers" element="Customers" />
+
+            {/*app */}
+            <Route path="/kaban" element="Kaban" />
+            <Route path="/editor" element="Editor" />
+            <Route path="/calendar" element="Calendar" />
+            <Route path="/color-picker" element="ColorPicker" />
+
+            {/*charts */}
+            <Route path="/line" element="Line" />
+            <Route path="/area" element="Area" />
+            <Route path="/bar" element="Bar" />
+            <Route path="/pie" element="Pie" />
+            <Route path="/financial" element="Financial" />
+            <Route path="/color-mapping" element="ColorMapping" />
+            <Route path="/pyramid" element="Pyramid" />
+            <Route path="/stacked" element="Stacked" />
+          </Routes>
         </div>
       </BrowserRouter>
     </div>
