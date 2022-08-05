3d2179cbf0e9060afd0f851a4a5a2815e183971b
willcordeiro <willcordeiro800@gmail.com>
2022-08-05 20:08:31 -0300

    added sidebar

--- src/App.js
+++ src/App.js
@@ -64,11 +64,11 @@ const App = () => {
             <Route path="/" element={<Ecommerce />} />
             <Route path="/ecommerce" element={<Ecommerce />} />
             {/*pages */}
-            <Route path="/orders" element={<Orders />} />{" "}
+            <Route path="/orders" element={<Orders />} />
             <Route path="/employees" element={<Employees />} />
             <Route path="/customers" element={<Customers />} />
             {/*app */}
-            <Route path="/kanban" element={<Kanban />} />{" "}
+            <Route path="/kanban" element={<Kanban />} />
             <Route path="/editor" element={<Editor />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/color-picker" element={<ColorPicker />} />
