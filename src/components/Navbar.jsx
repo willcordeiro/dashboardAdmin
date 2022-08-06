d5a7910e2161576e2f7b17377fd28bd86f3ddca9
willcordeiro <willcordeiro800@gmail.com>
2022-08-06 19:19:17 -0300

    added navbar imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { BsChatRight } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboeardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import avatar from "../data/data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+  return <div className="flex justify-between p-2 md:mx-6"></div>;
 };
 
 export default Navbar;
