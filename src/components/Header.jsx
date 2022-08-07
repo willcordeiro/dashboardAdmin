ddc2c9af26f6d0ab2e36835b156c9bd5cc031608
willcordeiro <willcordeiro800@gmail.com>
2022-08-07 08:54:35 -0300

    added orders section features and responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ category, title }) => {
   return (
     <div className="mb-10">
-      <p className="text-gray-400">{category}</p>
+      <p className="text-lg text-gray-400">{category}</p>
       <p className="text-3xl font-extrabold tracking-tight text-slate-900">
         {title}
       </p>
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -18,14 +18,31 @@ import { Header } from "../components";
 
 const Orders = () => {
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent id="gridcomp" dataSource={ordersData}>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
       </GridComponent>
     </div>
   );
