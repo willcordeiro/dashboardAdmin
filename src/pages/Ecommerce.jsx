0ff4a85ea7fcd8331a2ce8e8d6238cb441cc91eb
willcordeiro <willcordeiro800@gmail.com>
2022-08-06 21:14:55 -0300

    revenue section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -56,7 +56,23 @@ const Ecommerce = () => {
       <div className="flex gap-10 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
           <div className="flex justify-between">
-            <p> Revenue Update</p>{" "}
+            <p className="font-semibold text-xl">Revenue Update</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+            </div>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
